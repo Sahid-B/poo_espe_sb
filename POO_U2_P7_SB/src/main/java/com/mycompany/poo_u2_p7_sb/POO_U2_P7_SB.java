77546bec8b47e92495d840143b163d0f903ead24
Sahid-B <132688721+Sahid-B@users.noreply.github.com>
2023-08-27 17:06:10 -0500

    Segundo Parcial

---
+++ POO_U2_P7_SB/src/main/java/com/mycompany/poo_u2_p7_sb/POO_U2_P7_SB.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.poo_u2_p7_sb;
+
+/**
+ *
+ * @author ESPE
+ */
+public class POO_U2_P7_SB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
